31f85d6ea58b14632134c0367f999314015ba0e9
pancongcong <pancongcong@daojia.com>
2018-04-03 19:31:47 +0800

    清风徐来，水波不兴

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,34 @@
 <template>
-  <div id="app">
-    <search-bar></search-bar>
-    <div class="app-content">
-      <base-card title="当前定位">
+  <div id="app" class="app">
+    <div class="app-head">
+      <search-bar></search-bar>
+    </div>
+    <div class="app-body">
+      <div class="app-wrapper">
+        <base-card title="当前定位">
+          <div>
+            <base-button :text="currentCity"></base-button>
+          </div>
+        </base-card>
+        <base-card title="热门城市">
+          <div>
+            <base-button :text="currentCity"></base-button>
+            <base-button :text="currentCity"></base-button>
+            <base-button :text="currentCity"></base-button>
+            <base-button :text="currentCity"></base-button>
+            <base-button :text="currentCity"></base-button>
+            <base-button :text="currentCity"></base-button>
+            <base-button :text="currentCity"></base-button>
+            <base-button :text="currentCity"></base-button>
+          </div>
+        </base-card>
         <div>
-          <base-button :text="currentCity"></base-button>
+          <city-list :cityList="cityList"></city-list>
         </div>
-      </base-card>
-      <base-card title="热门城市">
-        <div>
-          <base-button :text="currentCity"></base-button>
-          <base-button :text="currentCity"></base-button>
-          <base-button :text="currentCity"></base-button>
-          <base-button :text="currentCity"></base-button>
-          <base-button :text="currentCity"></base-button>
-          <base-button :text="currentCity"></base-button>
-          <base-button :text="currentCity"></base-button>
-          <base-button :text="currentCity"></base-button>
-        </div>
-      </base-card>
+      </div>
+      <div class="app-nav">
+        <city-nav :cityList="cityList"></city-nav>
+      </div>
     </div>
   </div>
 </template>
@@ -29,27 +39,93 @@ import BaseButton from './components/base/BaseButton'
 
 import SearchBar from './components/SearchBar'
 import CurrentCity from './components/CurrentCity'
+import CityNav from './components/CityNav'
+import CityList from './components/CityList'
 
 export default {
   name: 'App',
   data () {
     return {
-      currentCity: '北京'
+      currentCity: '北京',
+      cityList: require('./assets/json/cityList.json')
+    }
+  },
+  created () {
+    this.cityList = this.handleCityList(this.cityList)
+  },
+  methods: {
+    handleCityList (cityList) {
+      let map = {}
+      cityList.forEach((item) => {
+        if (item.initials) {
+          item.initials = item.initials.toUpperCase()
+          if (typeof map[item.initials] === 'undefined') {
+            map[item.initials] = []
+          }
+          map[item.initials].push(item)
+        }
+      })
+      let result = []
+      for (let key in map) {
+        result.push({
+          initials: key,
+          cityList: map[key]
+        })
+      }
+      return result.sort((item1, item2) => {
+        return item1.initials > item2.initials ? 1 : -1
+      })
     }
   },
   components: {
     BaseCard,
     BaseButton,
     SearchBar,
-    CurrentCity
+    CurrentCity,
+    CityNav,
+    CityList
   }
 }
 </script>
 
-<style>
+<style lang="scss">
 @import url('./assets/css/reset.css');
 
-.app-content {
-  background: rgba(225,227,229,.54);
+.app {
+  position: relative;
+  height: 100%;
+  .app-head {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    background: #fff;
+  }
+  .app-body {
+    display: flex;
+    box-sizing: border-box;
+    height: 100%;
+    padding-top: 65px;
+    background: #f2f4f7;
+    .app-wrapper {
+      flex: 1;
+      overflow: scroll;
+    }
+    .app-nav {
+
+    }
+  }
 }
+// .content {
+//   display: flex;
+//   background: rgba(225,227,229,.54);
+//   .wrapper {
+//     flex: 1;
+//     overflow: hidden;
+//     height: 100%;
+//   }
+//   .nav {
+
+//   }
+// }
 </style>
---
+++ src/components/CityNav.vue
@@ -0,0 +1,46 @@
+<template>
+  <ul class="city-nav">
+    <li class="letter">
+      <div class="text">#</div>
+    </li>
+    <li class="letter" v-for="city in cityList" :key="city.initials"
+      @touchstart="touchstartHandle(city)">
+      <div class="text">{{ city.initials }}</div>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'CityNaV',
+  props: {
+    cityList: {
+      type: Array,
+      required: true
+    }
+  },
+  methods: {
+    touchstartHandle (city) {
+      this.$emit('touchLetterEvent', city)
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.city-nav {
+  padding-top: 64px;
+  .letter {
+    padding: 3px 0;
+    position: relative;
+    .text {
+      width: 13px;
+      height: 13px;
+      text-align: center;
+      line-height: 13px;
+      font-size: 12px;
+      font-weight: 700;
+    }
+  }
+}
+</style>
